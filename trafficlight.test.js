f86d8e5954c0ace59761243c57308a2519cd39eb
Yannick Bühler <buehlery@bzz.ch>
2024-12-06 13:37:10 +0100

    first commit

---
+++ trafficlight.test.js
@@ -0,0 +1,16 @@
+const app = require("./trafficlight");
+
+describe('test suite traffic light', () => {
+    it(`case red light`, () => {
+        expect(app.trafficLight("red")).toBe("DON'T WALK!");
+    });
+    it('case orange light', ()=> {
+        expect(app.trafficLight("orange")).toBe("ATTENTION!");
+    });
+    it('case green light', ()=> {
+        expect(app.trafficLight("green")).toBe("WALK!");
+    });
+    it('case blue light', ()=> {
+        expect(app.trafficLight("blue")).toBe(undefined);
+    });
+});
